fb3ffb1a5a6db6224ec487c2339d131112fc40c9
Mauro Vanetti <mauro_vanetti@docenti.naba.it>
2025-12-19 13:42:59 +0100

    Basic interaction

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
+using TMPro;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
     public GameObject[] targets;
     public float spawnInterval = 1.0f;
+    private int score = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -19,12 +24,13 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnInterval);
             int index = Random.Range(0, targets.Length);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void UpdateScore(int scoreToAdd)
     {
-        
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
